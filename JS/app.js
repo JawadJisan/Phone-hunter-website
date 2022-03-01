06c213f4c3ac9ff49fac3e622aeab80085a16d6f
JawadJisan <jisan.sc@gmail.com>
2022-03-01 13:13:03 +0600

    Extra Information added

--- JS/app.js
+++ JS/app.js
@@ -37,13 +37,14 @@ function getPhones(searchArea){
             phones.map(phone => 
                 {
                 console.log(phone.brand)
+                console.log(phone.slug)
                 const newDiv = document.createElement('div');
                 newDiv.className = 'col-md-3';
                 const phoneInfo = 
                     `
                         <a href="#clicked" class="text-decoration-none text-white">
 
-                        <div>
+                        <div id="clicked" onclick="displayDetails('${phone.slug}')" class="bg-info rounded text-center h-100" data-bs-toggle="modal" data-bs-target="#exampleModal">
 
                         <img class="img-fluid rounded-top" src="${phone.image}" alt="">
 
@@ -65,5 +66,32 @@ function getPhones(searchArea){
             warning.style.display = 'block';
         }
     };
+
+    const displayDetails = id =>{
+        const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+        fetch(url)
+        .then(res => res.json())
+        .then(data => displayPhoneInfo(data.data))
+    };
+
+    const displayPhoneInfo = phone =>{
+        console.log(phone)
+        const phoneDetailsDiv = document.getElementById('phoneDetails');
+
+        phoneDetailsDiv.innerHTML = `
+            <img class="img-fluid rounded mb-4" src="${phone.image}" alt="">
+            <h4>Name: ${phone.name}</h4>
+            <h4>Release Date: ${phone.releaseDate}</h4>
+            <h3 class="pt-3">Main Features </h3>
+            <ul class="list-unstyled mb-0">
+            <li>✅ChipSet: ${phone.mainFeatures.chipSet}</li>
+            <li>✅Display Size:${phone.mainFeatures.displaySize}</li>
+            <li>✅Memomry: ${phone.mainFeatures.memory}</li>
+            <li>✅Storage${phone.mainFeatures.storage}</li>
+            <li>✅Sensors: ${phone.mainFeatures.sensors}</li>
+        
+        
+        `
+    }
 
  
